516fe9cff329256362210db881a4067a5f488015
joel <bonjourjoel@gmail.com>
2024-02-15 09:55:09 +0100

    homepage design

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,15 +1,18 @@
-import Links from "./links/Links"
-import styles from "./navbar.module.css"
+import Link from "next/link";
+import Links from "./links/Links";
+import styles from "./navbar.module.css";
 
 const Navbar = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.logo}>Logo</div>
+      <Link href="/" className={styles.logo}>
+        Logo
+      </Link>
       <div>
         <Links />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
