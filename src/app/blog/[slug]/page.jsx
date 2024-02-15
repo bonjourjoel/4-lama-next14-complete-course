707a6e36d9860c3b4ef3c851f864a85f640935ae
joel <bonjourjoel@gmail.com>
2024-02-15 14:08:44 +0100

    blog page design

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,7 +1,40 @@
+import Image from "next/image";
+import styles from "./singlePost.module.css";
+
 const SinglePostPage = () => {
   return (
-    <div>SinglePostPage</div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image className={styles.img} src="/noavatar.png" alt="" fill />
+      </div>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Title</h1>
+        <div className={styles.detail}>
+          <Image
+            className={styles.avatar}
+            src="/noavatar.png"
+            alt=""
+            width={50}
+            height={50}
+          />
+          <div className={styles.detailText}>
+            <span className={styles.detailTitle}>Author</span>
+            <span className={styles.detailValue}>Terry Jefferson</span>
+          </div>
+          <div className={styles.detailText}>
+            <span className={styles.detailTitle}>Published</span>
+            <span className={styles.detailValue}>01.01.2024</span>
+          </div>
+        </div>
+        <div className={styles.content}>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora
+          eligendi distinctio quia facilis expedita ullam inventore nulla rerum
+          excepturi doloribus officia, quidem ad aut tenetur. Ad minus
+          temporibus iste atque.
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default SinglePostPage;
---
+++ src/components/postCard/postCard.jsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import styles from "./postCard.module.css";
+import Link from "next/link";
+
+const PostCard = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <div className={styles.imgContainer}>
+          <Image src="/noavatar.png" alt="" fill className={styles.img} />
+        </div>
+        <span className={styles.date}>01.01.2024</span>
+      </div>
+      <div className={styles.bottom}>
+        <h1 className={styles.title}>Title</h1>
+        <p className={styles.desc}>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla
+          voluptatum doloremque sunt omnis quibusdam magni beatae! Tempore
+          libero laudantium quia! Eveniet nulla ex itaque corrupti? Adipisci,
+          distinctio tempore. Officia, voluptate?
+        </p>
+        <Link className={styles.link} href="/blog/post">
+          READ MORE
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default PostCard;
