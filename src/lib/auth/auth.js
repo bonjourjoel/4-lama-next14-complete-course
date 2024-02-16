2b7f4aaf8de3a216d75681c80f970fab720b5e50
joel <bonjourjoel@gmail.com>
2024-02-16 19:59:13 +0100

    record OAuth user in our db

--- src/lib/auth/auth.js
+++ src/lib/auth/auth.js
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
+import { connectToDb } from "../utils";
+import { User } from "../models";
 
 export const {
   handlers: { GET, POST },
@@ -13,4 +15,28 @@ export const {
       clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      if (account.provider == "github") {
+        connectToDb();
+        try {
+          // check if the 0Auth signed in user already has an account in OUR database
+          const user = await User.findOne({ email: profile.email });
+          // if not, create one
+          if (!user) {
+            const newUser = new User({
+              username: profile.login, // profile.xxx fields names changes depending on the provider
+              email: profile.email,
+              img: profile.avatar_url,
+            });
+            await newUser.save();
+          }
+        } catch (error) {
+          console.log(error);
+          return false;
+        }
+      }
+      return true;
+    },
+  },
 });
